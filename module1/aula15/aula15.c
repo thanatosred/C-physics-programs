288374609a19d220373a9da001693e1e2dac426b
thanatos2 <damatooliveira@gmail.com>
2022-09-15 16:49:16 -0300

    aula15

---
+++ module1/aula15/aula15.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+//Integração numérica de X^2
+void main(){
+//metodo do retangulo
+double x,y,dx;
+//integra x^2 de 0 a 1
+dx = 0.001;
+y = 0.;
+for(x = 0; x<=1.;x+=dx){
+  y+= (x*x)*dx;
+}
+printf("Solucao numerica: %13.8f \n Solucao exata: %13.8f \n",y,1./3.);
+}
