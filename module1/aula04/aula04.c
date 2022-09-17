9a603758dbc0d3d5e766d019cfbf2d65d952e0c6
thanatos <damatooliveira@gmail.com>
2022-09-16 22:46:35 -0300

    adicionando aula04+figura

---
+++ module1/aula04/aula04.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+//Física Computacional 1 (V4)- Introdução geral (p4)
+
+void main(){
+
+    FILE *file;
+    char filename[] = "mapaLogistico.dat";
+    file = fopen(filename,"w");
+
+    double x2,x1,x0,r;
+    int n;
+    
+    for (r=2.75;r<=3.99;r=r+0.001){
+        for(x0 = 0.01; x0<=0.99;x0 =x0+0.02){
+            x1=x0;
+            for(n=1.;n<=10000;n=n+1){
+                x2 = r*x1*(1.-x1);
+                x1 =x2;
+            }
+            fprintf(file,"%13.8f %13.8f\n",r,x1);
+        }
+    }
+    
+}
