3df46fa614d8f9e1933c9a3ed5336b5a2e723693
thanatos <damatooliveira@gmail.com>
2022-09-17 19:07:25 -0300

    aula11 pronta

--- module1/aula06/aula06.c
+++ module1/aula06/aula06.c
@@ -3,31 +3,35 @@
 //Física Computacional 1 - Introdução geral parte 6 - Calculando a curva média de tabela de dados
 void curvaMediaArit(int n){
 // n é o número de intervalos computados
-FILE *partivideo06,*file;
+/*FILE *partivideo06,*file;
 partivideo06 = fopen("partivideo6.dat","rb");
 file = fopen("curvaMediaArti.dat","w");
-double x[] = { };
-double y[] = { };
-double a,b,xs,ys,dx = 0.;
+double x[10010] = { };
+double y[10010] = { };
+double xx,xs,ys,ym,dx = 0.;
 dx = 0.1;
+int cont,r1 = 0;
 //leitura dos arquivos
-int c =0;
-for (int i =0; fscanf(partivideo06,"%lf %lf",&a,&b)!=EOF;i++){//a condição ler o arquivo até não encontrar mais linhas para leitura (EOF = -1)
-    x[i]=i*0.1;
-    y[i]=i*0.1;
-    printf("i:%d\n",i);
+for (int i =0; fscanf(partivideo06,"%lf %lf",&x[i],&y[i])!=EOF;i++){//a condição ler o arquivo até não encontrar mais linhas para leitura (EOF = -1)
+    cont++;
 }
-/*for (x = -2.6; x <=2.5; x +=dx){
-}*/
+//
+for (xx = -2.6; xx <=2.5; xx +=dx){
+    for(int i = 0; i<cont;i++){
+        if(abs(xx-x[i])<dx){
+            r1+=1;
+            ys+=y[i];
+        }
+        if(r1>0){
+            ym = ys/r1;
+            //fprintf(file,"%lf %lf\n",x[r1],ym);
+            printf("xm: %lf ym: %lf i: %d\n",x[i],ym,i);
+        }
+    }
+}
 fclose(partivideo06);
+*/
 }
 void main(){
     curvaMediaArit(10);
-/*double x[]={ };
-for (int i = 0; i < 20.; i++)
-{
-    x[i] = i*0.1;
-    printf("x: %lf \n",x[i]);
-}*/
-
 }
