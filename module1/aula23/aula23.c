d82cd312d30cd24e9913d6ec09d5946782a17904
thanatos <damatooliveira@gmail.com>
2022-10-12 10:34:31 -0300

    aula23-full

--- module1/aula23/aula23.c
+++ module1/aula23/aula23.c
@@ -34,7 +34,7 @@ int histograma(){
     FILE *file,*file2,*file3;
     char filename[100];
     char filename2[100];
-    char filename3[100] = "histograma_nor";
+    char filename3[100] = "histograma_nor.dat";
 
 
     printf("Digite o nome do arquivo de leitura \n");
@@ -44,17 +44,20 @@ int histograma(){
 
     file = fopen(filename,"r");
     file2 = fopen(filename2,"w");
-    file2 = fopen(filename3,"w");
+    file3 = fopen(filename3,"w");
 
     int i,j,k,cont,Nmax;
     Nmax = 200000;
     double data[Nmax];
     double r1,r2,x,rmin,rmax,dx;
 
-    double norma[150],xc [150];
+    double norma[200],xc [200];
     //leitura do arquivo
-    for(cont=1; fscanf(file,"%lf",&data[cont])!=EOF;cont++){
+    double leit;
+    for(cont=1; fscanf(file,"%lf",&leit)!=EOF;cont++){
+        data[cont] = leit;
     }
+    printf("lido: %lf \n",data[1]);
 
     rmax = -100.;
     rmin = 100.;
@@ -68,35 +71,38 @@ int histograma(){
         }
     }
     dx = (rmax-rmin)/100.;
+    
+    printf("rmax: %lf \n rmin: %lf \n dx: %lf \n",rmax,rmin,dx);
 
-    j = 0;
+    j = 1;
     for(x=dx;x<=(rmax-dx);x+=dx){
         r1=0.;
         for(i=1;i<=cont;i++){
-            if(fabs(data[i]-x)<=0.5*dx){
+            if(fabs(data[i]-x)<0.5*dx){
                 r1+=1.;
             }
         }
         xc[j] = x;
         norma[j] = r1;
+        fprintf(file2,"%20.8g \n",r1);
         j++;
-        fprintf(file2,"%lf \n",r1);
     }
-
-    r2 = 0;
-    for(i = 1; i <= j-1; i ++){
-        r2 = r2 + (norma[i+1] + norma[i])*dx/2;
+    printf("Histograma gerado \n x: %lf \n r1: %lf \n",x,r1);
+    //integracao e histograma
+    r2 = 0.;
+    for(i = 1; i < j; i ++){
+        r2 += (norma[i+1] + norma[i])*dx/2;
     }
-    for(i = 1; i <= j-1; i ++){
+    for(i = 1; i < j; i ++){
         norma[i] = norma[i]/r2;
         fprintf(file3, "%20.8g %20.8g \n", xc[i], norma[i]);
     }
-    
+    printf("Histograma normalizado \n");
+
     return 0;
 }
 int main(){
     //gerar();
-    histograma();
     histograma();
     return 0;
 }
