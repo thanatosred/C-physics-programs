13808cc843d90ea3ad1282ce9d72b7cfe43b7ee3
thanatos2 <damatooliveira@gmail.com>
2022-10-20 03:58:42 -0300

    a38-v3

--- module1/aula38-Avaliativa/atividade-avaliativa.c
+++ module1/aula38-Avaliativa/atividade-avaliativa.c
@@ -18,26 +18,27 @@ int main(){
   int fgrade[N][N];
 
   int inf0 = N/2;
-  double prob,time;
+  double prob;
+  int time;
   prob = 0.5;
-  time = (double)N*0.1;
-  printf("valor: %lf \n",time);
+  time = N;
+  printf("valor: %i \n",time);
   
-  for(i=1; i<=N;i++){
-    for(j=1; i<=N; j++){
+  for(i=1; i<N;i++){
+    for(j=1; i<N; j++){
       grade[i][j] = 0;
       fgrade[i][j] = 0;
     }
   }
   //elemento infectado
   grade[inf0][inf0] = fgrade[inf0][inf0] = 1;
-  printf("valor: %lf \n",fgrade[inf0][inf0]);
-  for(double t = 0.; t<time;t+=0.1){
-    for(i=1;i<=N;i++){
-      for(j=1;j<=N;j++){
+  printf("valor: %i \n",fgrade[inf0][inf0]);
+  for(int t = 0; t<time;t++){
+    for(i=1;i<N;i++){
+      for(j=1;j<N;j++){
         if(grade[i][j] == 1){
-          for(int k = i-1;k<=i+1;k++){
-            for(int m = j-1;m<=j+1;m++){
+          for(int k = i-1;k<i+1;k++){
+            for(int m = j-1;m<j+1;m++){
               if(RAND()>prob){
                 if(grade[k][m]!=1 && j!=N && i!=N){
                   fgrade[k][m] = 1;
@@ -49,8 +50,8 @@ int main(){
       }
     }
     //substituindo a grade
-    for(i=1;i<=N;i++){
-      for(j=1;j<=N;j++){       
+    for(i=1;i<N;i++){
+      for(j=1;j<N;j++){       
         grade[i][j] = fgrade[i][j];
         fprintf(file,"%i \n",grade[i][j]);
       }
