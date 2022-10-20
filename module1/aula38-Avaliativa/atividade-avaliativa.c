ae1823d18effe032cd2d8c177cfcdc522d840f6b
thanatos2 <damatooliveira@gmail.com>
2022-10-20 04:10:50 -0300

    a38-v4

--- module1/aula38-Avaliativa/atividade-avaliativa.c
+++ module1/aula38-Avaliativa/atividade-avaliativa.c
@@ -23,22 +23,24 @@ int main(){
   prob = 0.5;
   time = N;
   printf("valor: %i \n",time);
-  
+
   for(i=1; i<N;i++){
     for(j=1; i<N; j++){
       grade[i][j] = 0;
       fgrade[i][j] = 0;
     }
   }
+  printf("valor grade: %i \n",grade[i][j]);
+
   //elemento infectado
   grade[inf0][inf0] = fgrade[inf0][inf0] = 1;
-  printf("valor: %i \n",fgrade[inf0][inf0]);
   for(int t = 0; t<time;t++){
     for(i=1;i<N;i++){
       for(j=1;j<N;j++){
+        //identificando contaminacao
         if(grade[i][j] == 1){
-          for(int k = i-1;k<i+1;k++){
-            for(int m = j-1;m<j+1;m++){
+          for(int k = i-1;k<=i+1;k++){
+            for(int m = j-1;m<=j+1;m++){
               if(RAND()>prob){
                 if(grade[k][m]!=1 && j!=N && i!=N){
                   fgrade[k][m] = 1;
@@ -50,8 +52,8 @@ int main(){
       }
     }
     //substituindo a grade
-    for(i=1;i<N;i++){
-      for(j=1;j<N;j++){       
+    for(i=1;i<=N;i++){
+      for(j=1;j<=N;j++){       
         grade[i][j] = fgrade[i][j];
         fprintf(file,"%i \n",grade[i][j]);
       }
